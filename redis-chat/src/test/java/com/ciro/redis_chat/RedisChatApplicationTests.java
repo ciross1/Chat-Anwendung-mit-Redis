c652e223fef3e3751362f19428936056f1170f8f
Ciro Santamarina <ciro.santamarina@bitec.es>
2026-05-27 12:33:54 +0200

    Zweiter commit

---
+++ redis-chat/src/test/java/com/ciro/redis_chat/RedisChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciro.redis_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
