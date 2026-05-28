a94c9cf98f13e3f4b2e1931a5e0607c161685c83
Ciro Santamarina <ciro.santamarina@bitec.es>
2026-05-28 10:36:17 +0200

    ChatMessage

--- redis-chat/src/main/java/com/ciro/redis_chat/chat/app/ChatMessage.java
+++ redis-chat/src/main/java/com/ciro/redis_chat/chat/app/ChatMessage.java
@@ -1,4 +1,54 @@
 package com.ciro.redis_chat.chat.app;
 
 public class ChatMessage {
+
+    private String user; // We schreibt?
+    private String message;
+    private String timestamp;
+
+
+    //Konstruktor
+    public ChatMessage(String user, String message, String timestamp){
+        this.user = user;
+        this.message = message;
+        this.timestamp = timestamp;
+
+    }
+
+
+    //Getter
+    public String getUser(){
+        return user;
+    }
+    public String getMessage(){
+        return message;
+    }
+
+    public String getTimestamp(){
+        return timestamp;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ChatMessage{" +
+                "user='" + user + '\'' +
+                ", message='" + message + '\'' +
+                ", timestamp='" + timestamp + '\'' +
+                '}';
+    }
 }
+
+
+/*Ohne ChatMessage:
+java// Nur ein einfacher String
+publish("Anna: Hallo!")
+// ← unstrukturiert, schwer zu verarbeiten
+
+
+Mit ChatMessage:
+java// Strukturiertes Objekt
+ChatMessage msg = new ChatMessage("Anna", "Hallo!", "10:30");
+msg.getUser();      // "Anna"
+msg.getMessage();   // "Hallo!"
+msg.getTimestamp(); // "10:30"*/
